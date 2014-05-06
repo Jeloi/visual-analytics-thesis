c2c2ed7f5ff77cfdb95d0d151c78e4b7004d73f8
Jeloi <jeremyho19@gmail.com>
2014-05-06 18:18:55 -0400

    searchMicroblogs now returns an array, w/ first element being the hour_counts type object and the second the binned ids. client must use these binned id's to hours_data equivalent for the search

--- server/search.js
+++ server/search.js
@@ -7,7 +7,7 @@ _searchMicroblogs = function (searchText) {
         search: searchText,
         limit: 1000000,
         project: {
-            hour_index: 1,
+          hour_index: 1,
           id: 1 // Only take the ids
         }
      }
@@ -22,16 +22,39 @@ _searchMicroblogs = function (searchText) {
     return future.wait();
 };
  
-// Helper that extracts the ids from the search results
+// Helper that extracts the ids from the search results. Returns an array w/ first element containing search counts and second element containing binned id's of search hits
 searchMicroblogs = function (searchText) {
     if (searchText && searchText !== '') {
         var searchResults = _searchMicroblogs(searchText);
         console.log(searchResults.length);
-        var ids = [];
+        var search_counts = [];
+        var search_results = {};
+
+        // Initialize counts of bins to 0
+        for (var i = 0; i < num_hours; i++) {
+            search_counts[i] = {hour_index: i, count: 0};
+        };
+
+        console.log(num_hours);
+
+        // Bin the search results by hour_index
         for (var i = 0; i < searchResults.length; i++) {
-            ids.push(searchResults[i].obj._id);
+            var hour_index = searchResults[i].obj.hour_index;
+
+            if (hour_index < num_hours) {
+                search_counts[hour_index].count = (search_counts[hour_index].count+1);
+            };
+
+            if (!(hour_index in search_results)) {
+                search_results[hour_index] = [searchResults[i].obj._id];
+            } else {
+                search_results[hour_index].push(searchResults[i].obj._id);
+            };
         }
         console.log("inside searchMicroblogs");
-        return ids;
+
+        // Initialize bins for hours that will each hold an array of id's
+        console.log(search_counts.length);
+        return [search_counts, search_results];
     }
 };
